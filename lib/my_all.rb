09c9b87af544ac3b252ccf9f860704c9949d2a92
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-07-30 18:13:11 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   i = 0
   block_returns_values = []
   while i < collection.length
-    yield(collection[i])
+    block_return_value << yield(collection[i])
     i += 1
   end
 end
